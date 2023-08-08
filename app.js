f9500bd8829052a2e904d0cb75486183328b18d9
Wellerson Ferreira de Carvalho <wellersonfc@gmail.com>
2023-08-08 20:03:43 -0300

    new - integracao Openia

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
 const port = 8000;
 
 app.get('/', (req, res) => {
-    res.send("");
+    const a = process.env.OPENAI_API_KEY;
+    console.log(a);
+    res.send(process.env.OPENAI_API_KEY);
 });
 
 app.listen(port, () => {
     console.log('listening on port');
 })
+
+
+
